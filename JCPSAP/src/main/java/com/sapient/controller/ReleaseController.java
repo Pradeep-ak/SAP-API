590cf57ab80f138c443573fa872e815353b5b7a3
PK <pradeep4jobs@gmail.com>
2017-01-11 00:48:57 -0600

    Second Commit

--- JCPSAP/src/main/java/com/sapient/controller/ReleaseController.java
+++ JCPSAP/src/main/java/com/sapient/controller/ReleaseController.java
@@ -1,5 +1,45 @@
 package com.sapient.controller;
 
-public class ReleaseController {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sapient.model.Release.ReleaseModel;
+
+@RestController
+@RequestMapping("/release")
+public class ReleaseController {
+	
+	private Map<String, ReleaseModel> releaseMap;
+	
+	@RequestMapping("")
+    @ResponseBody
+	public List<ReleaseModel> index() {
+		loadConfig();
+		return new ArrayList<>(releaseMap.values());
+	}
+	
+	@RequestMapping("/{id}")
+    @ResponseBody
+	public ReleaseModel getReleaseDetails(@PathVariable("id") String Id) {
+		loadConfig();
+		return releaseMap.get(Id);
+	}
+
+	private void loadConfig() {
+		if (releaseMap == null) {
+			releaseMap = new HashMap<String, ReleaseModel>();
+			releaseMap.put("1", new ReleaseModel("1", new Date(),"12.8",new ArrayList<String>(),new ArrayList<String>()));
+			releaseMap.put("2",  new ReleaseModel("2", new Date(),"12.9",new ArrayList<String>(),new ArrayList<String>()));
+			releaseMap.put("3",  new ReleaseModel("3", new Date(),"12.10",new ArrayList<String>(),new ArrayList<String>()));
+			releaseMap.put("4",  new ReleaseModel("4", new Date(),"12.11",new ArrayList<String>(),new ArrayList<String>()));
+		}
+	}
 }
---
+++ JCPSAP/src/main/java/com/sapient/model/Release/ReleaseModel.java
@@ -0,0 +1,68 @@
+package com.sapient.model.Release;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ReleaseModel {
+
+	private String id;
+	
+	@JsonFormat(pattern="MM/dd/yyyy")
+	private Date releaseDate;
+	private String releaseNumber;
+	private List<String> sprints;
+	private List<String> majorChanges;
+	
+	public ReleaseModel(String id, Date releaseDate, String releaseNumber, List<String> sprints,
+			List<String> majorChanges) {
+		super();
+		this.id = id;
+		this.releaseDate = releaseDate;
+		this.releaseNumber = releaseNumber;
+		this.sprints = sprints;
+		this.majorChanges = majorChanges;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getReleaseNumber() {
+		return releaseNumber;
+	}
+
+	public void setReleaseNumber(String releaseNumber) {
+		this.releaseNumber = releaseNumber;
+	}
+
+	public List<String> getSprints() {
+		return sprints;
+	}
+
+	public void setSprints(List<String> sprints) {
+		this.sprints = sprints;
+	}
+
+	public List<String> getMajorChanges() {
+		return majorChanges;
+	}
+
+	public void setMajorChanges(List<String> majorChanges) {
+		this.majorChanges = majorChanges;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
